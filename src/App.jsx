1f09bec0f423b2a0db6551813df8a1237ea279c8
Krosto4 <voronstars1255@gmail.com>
2024-08-01 17:55:01 +0400

    final-project_pages-links

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Home from "./pages/Home";
+import Shop from "./pages/Shop";
+import ErrorPage from "./pages/ErrorPage";
+import LogIn from "./pages/LogIn";
+import Registration from "./pages/Registration";
+import Profile from "./pages/Profile";
 
 function App() {
   return (
-  <>
-    App
-  </>
-  )
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<ErrorPage/>}/>
+        <Route path="/shop" element={<Shop />} />
+        <Route path="/log-in" element={<LogIn/>}/>
+        <Route path="/registration" element={<Registration/>}/>
+        <Route path="/profile" element={<Profile/>}/>
+      </Routes>
+    </>
+  );
 }
 
 export default App;
---
+++ src/pages/ErrorPage.jsx
@@ -0,0 +1,7 @@
+export default function ErrorPage(){
+    return (
+        <>
+            Error 404: Your URL is uncorrect
+        </>
+    )
+}
